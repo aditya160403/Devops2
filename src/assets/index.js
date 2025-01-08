d713fcf57a41dccc8fd2869ed0cc7dbb44a679d3
aditya160403 <solankeaditya31@gmail.com>
2025-01-08 15:46:13 +0530

    new photo

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,7 @@ import worldmap from './backgrounds/world-map.png';
 import logo from './logo/logo-black.png';
 import logotext from './logo/logo-text-black.png';
 
-import shaq from './personal/adi2(2).png';
+import shaq from './personal/adi2 3.png';
 
 import backend from './icons/backend.png';
 import ux from './icons/ux.png';
